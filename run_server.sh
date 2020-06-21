3dcb532016c7fbf48c200e6e7dd92173302cd4fb
root <root@calm-poke-1.localdomain>
2020-06-21 10:22:33 -0400

    Eden changes

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-docker run --rm -d -e "CADDYPATH=/etc/caddycerts" -v $HOME/.caddy:/etc/caddycerts -v `pwd`/Caddyfile:/etc/Caddyfile -v `pwd`/wwwroot:/wwwroot -p 80:80 -p 443:443 abiosoft/caddy
+#docker run --rm -d -e "CADDYPATH=/etc/caddycerts" -v $HOME/.caddy:/etc/caddycerts -v `pwd`/Caddyfile:/etc/Caddyfile -v `pwd`/wwwroot:/wwwroot -p 80:80 -p 443:443 abiosoft/caddy
+docker run --rm -d -e "CADDYPATH=/etc/caddycerts" -v $HOME/.caddy:/etc/caddycerts -v `pwd`/Caddyfile:/etc/Caddyfile -v `pwd`/wwwroot:/wwwroot -p 443:443 abiosoft/caddy
 docker run --rm -d -v `pwd`/server.json:/etc/v2ray/config.json -p 1108:1108 v2ray/official
